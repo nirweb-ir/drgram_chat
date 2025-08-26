5286af57c03d8d9f7c8ec5d79d2d3de5e150e99a
ahmady-nirweb <ahmady.nirweb@gmail.com>
2025-08-26 10:16:21 +0330

    1.1.0.4

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <html lang="fa" dir="rtl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+
     <title> پذیرش چت </title>
     <link rel="stylesheet" href="./assets/css/chat.css?v=1.1.0">
     <link rel="stylesheet" href="./assets/font/fontiran.css">
