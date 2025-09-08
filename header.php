e9f931675cc095162f81bf99fca7b6a17927b80e
nirweb-ir <nirweb.ir@gmail.com>
2025-09-08 14:16:21 +0330

    1.1.0.21

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
 
     <title> پذیرش چت </title>
-    <link rel="stylesheet" href="./assets/css/chat.css?v=1.1.1.3">
+    <link rel="stylesheet" href="./assets/css/chat.css?v=1.1.2">
     <link rel="stylesheet" href="./assets/font/fontiran.css">
     <script>
         const ajaxUrl = "<?php echo 'functions/ajax.php'; ?>";
